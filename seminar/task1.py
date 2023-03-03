3eff753e188be4ff3a08ba48ed8bb0f22481c9e3
Pavel <dispitcher@mail.ru>
2023-03-03 17:24:31 +0300

    уникальные значения списка

--- seminar/task1.py
+++ seminar/task1.py
@@ -1,1 +1,10 @@
-print(5)
+#Дан список чисел. Определить сколько уникальных значений
+
+counter = int(input("Сколько элементов списка?: "))
+numbers = []
+for i in range(counter):
+    numbers.append(int(input("введите число ")))
+print(numbers)
+num_value = len(set(numbers))
+print(num_value)
+    
